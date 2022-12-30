78125624171cce42fbba56988ec25cbb410f6baa
davitorress <dev.davitorres@gmail.com>
2022-12-30 14:55:43 -0300

    working with portals

--- add-user-project/src/components/UI/ErrorModal.js
+++ add-user-project/src/components/UI/ErrorModal.js
@@ -1,26 +1,40 @@
+import ReactDOM from "react-dom";
+
 import Card from "./Card";
 import Button from "./Button";
 
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+	return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<Card className={styles.modal}>
+			<header className={styles.header}>
+				<h2>{props.title}</h2>
+			</header>
+
+			<section className={styles.content}>
+				<p>{props.message}</p>
+			</section>
+
+			<footer className={styles.actions}>
+				<Button onClick={props.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
+
 const ErrorModal = (props) => {
 	return (
 		<>
-			<div className={styles.backdrop} onClick={props.onConfirm} />
-
-			<Card className={styles.modal}>
-				<header className={styles.header}>
-					<h2>{props.title}</h2>
-				</header>
-
-				<section className={styles.content}>
-					<p>{props.message}</p>
-				</section>
-
-				<footer className={styles.actions}>
-					<Button onClick={props.onConfirm}>Okay</Button>
-				</footer>
-			</Card>
+			{ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.querySelector("#backdrop-root"))}
+			{ReactDOM.createPortal(
+				<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />,
+				document.querySelector("#overlay-root")
+			)}
 		</>
 	);
 };
