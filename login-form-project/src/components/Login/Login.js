625444b4d7955a65d96fc1fb5b1a1d2d4d06372b
davitorress <dev.davitorres@gmail.com>
2023-01-02 16:53:17 -0300

    refactoring an input component

--- login-form-project/src/components/Login/Login.js
+++ login-form-project/src/components/Login/Login.js
@@ -5,6 +5,7 @@ import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
 import AuthContext from "../../context/auth-context";
+import Input from "../UI/Input/Input";
 
 const Login = (props) => {
 	const ctx = useContext(AuthContext);
@@ -68,26 +69,24 @@ const Login = (props) => {
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
-				<div className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ""}`}>
-					<label htmlFor="email">E-Mail</label>
-					<input
-						type="email"
-						id="email"
-						value={emailState.value}
-						onChange={emailChangeHandler}
-						onBlur={validateEmailHandler}
-					/>
-				</div>
-				<div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ""}`}>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						id="password"
-						value={passwordState.value}
-						onChange={passwordChangeHandler}
-						onBlur={validatePasswordHandler}
-					/>
-				</div>
+				<Input
+					id="email"
+					type="email"
+					label="E-mail"
+					value={emailState.value}
+					isValid={emailIsValid}
+					onChange={emailChangeHandler}
+					onBlur={validateEmailHandler}
+				/>
+				<Input
+					id="password"
+					type="password"
+					label="Password"
+					value={passwordState.value}
+					isValid={passwordIsValid}
+					onChange={passwordChangeHandler}
+					onBlur={validatePasswordHandler}
+				/>
 				<div className={classes.actions}>
 					<Button type="submit" className={classes.btn} disabled={!formIsValid}>
 						Login
