1f32866e41acff6a88da9aa74c7a480446dbd375
davitorress <dev.davitorres@gmail.com>
2023-01-11 14:51:49 -0300

    react router v6

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,4 +1,4 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
@@ -11,23 +11,16 @@ function App() {
 		<div>
 			<MainHeader />
 			<main>
-				<Switch>
-					<Route path="/" exact>
-						<Redirect to="/welcome/new-user" />
+				<Routes>
+					<Route path="/" element={<Navigate to="/welcome/new-user" />} />
+
+					<Route path="/welcome/*" element={<Welcome />}>
+						<Route path="new-user" element={<p>Welcome, new user!</p>} />
 					</Route>
 
-					<Route path="/welcome">
-						<Welcome />
-					</Route>
-
-					<Route path="/products" exact>
-						<Products />
-					</Route>
-
-					<Route path="/products/:productId">
-						<ProductDetail />
-					</Route>
-				</Switch>
+					<Route path="/products" element={<Products />} />
+					<Route path="/products/:productId" element={<ProductDetail />} />
+				</Routes>
 			</main>
 		</div>
 	);
